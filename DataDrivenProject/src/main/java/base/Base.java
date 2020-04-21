a91668fe3a5fcbeb93b962ecf953ea7f099522f0
Swapnil <swapnil.kale@live.com>
2020-04-21 19:49:41 +0530

    Data Driven Framework

---
+++ DataDrivenProject/src/main/java/base/Base.java
@@ -0,0 +1,9 @@
+package base;
+
+import utilities.ExcelReader;
+
+public class Base {
+	
+	public static ExcelReader excel = new ExcelReader(System.getProperty("user.dir") + "\\src\\test\\resources\\excel\\testdata.xlsx");
+
+}
