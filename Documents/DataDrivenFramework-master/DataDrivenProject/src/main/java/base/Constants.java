9a3325fc122dccf7d121e2c18ad4d9f0af2e0e1e
swapnilk21 <swapnil.kale@live.com>
2021-06-26 14:43:59 +0530

    Updated framework structure

---
+++ Documents/DataDrivenFramework-master/DataDrivenProject/src/main/java/base/Constants.java
@@ -0,0 +1,10 @@
+package base;
+
+public class Constants {
+
+	//Configuration
+	public static final String browser = "chrome";
+	public static final String testsiteurl = "http://expedia.co.in";
+	public static long implicitwait=10;
+	
+}
