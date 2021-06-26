026bd9e34aed179627c3937e5e6530ff96f1fb75
swapnilk21 <swapnil.kale@live.com>
2021-06-26 15:01:15 +0530

    added source folder

---
+++ Documents/DataDrivenFramework-master/DataDrivenProject/src/test/java/tests/Test1.java
@@ -0,0 +1,26 @@
+package tests;
+
+import org.testng.annotations.Test;
+import java.util.Hashtable;
+
+import utilities.TestUtil;
+
+public class Test1 {
+	
+	@Test(dataProviderClass=TestUtil.class, dataProvider="dp")
+	public void test1(Hashtable<String,String> data) {
+		
+		System.out.println(data.get("Ref_ID"));
+		System.out.println(data.get("Security"));
+		System.out.println(data.get("Buy_Sell"));
+		System.out.println(data.get("Settlement_Date"));
+		System.out.println(data.get("Trade_Date"));
+		System.out.println(data.get("Commission"));
+		System.out.println(data.get("Commission_Type"));
+		System.out.println(data.get("Quantity"));
+		System.out.println(data.get("Price"));
+		System.out.println("\n===============================================\n");
+		
+	}
+
+}
